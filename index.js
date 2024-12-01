657012631cf2ce9200622943a3ce72251d016517
Jatin Mamtora <91244035+Ja3mamtora@users.noreply.github.com>
2024-12-01 21:07:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ app.post('/login', async (req, res, next) => {
         }
 
         const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET);
-        res.cookie('token', token);
+        res.cookie('token', token, { httpOnly: true, sameSite: 'none', secure: true });
         res.json({ message: 'Login successful', user });
     } catch (error) {
         console.log(error);
